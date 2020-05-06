92dc2cf9952f02edccbb1ce9e9ca5b2701e84074
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-06 12:37:16 -0400

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,1 +1,11 @@
+require('dotenv').config();
+const mysql = require('mysql2');
 
+mysql.createConnection({
+ user: process.env.DB_USER,
+ password: process.env.DB_PASS
+}).then((connection) => {
+  connection.execute(`CREATE TABLE Guilds(guildID VARCHAR(255))`)
+})
+.then(() => console.log('Good'))
+.catch(err => console.log(err));
