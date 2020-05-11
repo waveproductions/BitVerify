4cfd0fdddb451be168c5ba06702956dd16721d3c
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-11 11:37:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -44,38 +44,4 @@ bot.on("message", async message => {
 
 })
 
-bot.on('ready', () => {
-  console.log('✅|Ready') +
-  bot.user.setStatus("online"); 
-  
-  let statuses = [
-   `v!help`,
-   `with roles`,
-  ];
-  
-  setInterval(function() {
-      let status = statuses[Math.floor(Math.random() * statuses.length)];
-      bot.user.setActivity(status, {type: "PLAYING"})
-  }, 2000);
-
-});
-
-//Prefix for commands
-const PREFIX = 'v!';
-
-bot.on("message", async message => {
-    let prefix = PREFIX;
-    if(message.author.bot) return;
-    if(message.channel.type === 'dm') return;
-    if(message.content.indexOf(prefix) !== 0) return;
-    let content = message.content.split(" ");
-    let command = content[0];
-    let args = content.slice(1);
-  
-  
-    //checks if message contains a command and runs it
-    let commandfile = bot.commands.get(command.slice(prefix.length));
-    if(commandfile) commandfile.run(bot,message,args);
-  })
-
 bot.login(process.env.token)
