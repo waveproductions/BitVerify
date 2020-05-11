fe500d17bfb7746e805086f57cadc1445d644fd8
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-11 11:43:45 -0400

    Create database.js

---
+++ database/database.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose');
+module.exports = mongoose.connect('mongodb+srv://bitverify:63asdfpee1@cluster0-opjfq.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true});
