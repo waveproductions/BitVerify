171b042c8773c8835342fbf731555997ccbb601a
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-11 17:04:16 -0400

    Delete database.js

--- database/database.js
+++
@@ -1,2 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = mongoose.connect('mongodb+srv://bitverify:63asdfpee1@cluster0-opjfq.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true});
