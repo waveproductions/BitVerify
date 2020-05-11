bfb7ad67ef12fd7fcd261f8c97d28dbe4b157967
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-11 11:33:44 -0400

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,17 @@
+  const Discord = require('discord.js');
+  
+  module.exports = bot => {
+  console.log(`✅|${bot.user.username} is up and working`) +
+  bot.user.setStatus("online"); 
+  
+  let statuses = [
+   `v!help`,
+   `with roles.`
+  ];
+  
+  setInterval(function() {
+      let status = statuses[Math.floor(Math.random() * statuses.length)];
+      bot.user.setActivity(status, {type: "PLAYING"});
+  }, 2000);
+
+};
