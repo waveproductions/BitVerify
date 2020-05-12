5ca07f5e1c8bdf70b4bd61b6848ac17c5fcad294
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-12 14:48:59 -0400

    Create verifyinstructions.js

---
+++ commands/verifyinstructions.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+  if(!message.member.hasPermission('ADMINISTRATOR')) {
+  return message.channel.send('You don\'t have permission to use this command.')
+  }
+  let helpEmbed = new Discord.MessageEmbed()
+  .setTitle('BitVerify Instructions')
+  .setDescription(`Say v!settings and do what I tell you. Then your bot should be ready to go! \:D`)
+  .setFooter('The prefix is v!')
+  .setColor('GREEN')
+  message.channel.send(helpEmbed)
+}
+
+module.exports.config = {
+    name: "verifyinstructions",
+    description: "Instructions for the bot.",
+    usage: "v!verifyinstructions",
+    accessableby: "Admins",
+    aliases: ['']
+}
