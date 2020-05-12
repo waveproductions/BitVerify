28de43bff44603e92963c31a2cd6a1a433d0356d
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-12 18:32:25 -0400

    Update verifymessage.js

--- commands/verifymessage.js
+++ commands/verifymessage.js
@@ -10,8 +10,15 @@ guildSettings.findOne({ GuildID: message.guild.id }, async(err, data) => {
   if(!data) {
   return message.channel.send('You haven\'t set your verify channel and role yet. Say \`v!settings\` to do it now!')
   }
-  
-  let sendchannel = message.guild.channels.cache.find(data.VerifyChannelID)
+  let argsEmbed = new Discord.MessageEmbed()
+  .setTitle('Not Enough Args')
+  .setDescription(`❌ Please be more descriptive.`)
+  .addField('Example:', '\`v!verifymessage <channel mention>\`')
+  .setColor('RED')
+  let sendchannel = message.mentions.channels.first()
+  if(!sendchannel) {
+  return message.channel.send('You didn\'t mention a channel!')
+  }
   let embed = new Discord.MessageEmbed()
   .setTitle('Verification')
   .setDescription(`To become a member of ${message.guild.name}, say \`v!verify\` in this channel.`)
