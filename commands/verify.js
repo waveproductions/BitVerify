b0a314ad1da1e133bded26a7b9aa826a95f2544b
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-12 10:48:26 -0400

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -1,19 +1,23 @@
 const Discord = require('discord.js')
 const mongoose = require('mongoose')
-const guildSettings = require('../models/GuildCreate')
 
 module.exports.run = async (bot, message, args) => {
-  if(message.deletable) message.delete();
+  const guildSettings = require('../models/GuildCreate')
   guildSettings.findOne({ GuildID: message.guild.id }, async(err, data) => {
+  if(message.author.bot) return;
+  if(message.channel.id === data.VerifyChannelID) {
+  await message.delete().catch(err => console.log(err));
   let role = data.VerifiedRoleID
-  if(err) console.log(err)
-  
-  if(!data) {
-  return message.channel.send('The verified role and channel hasn\'t been set up, please contact an Administrator.')
-  }
-      
-  if(message.channel.id !== data.VerifiedChannelID) return;
-  message.guild.member.roles.add(role)
+  if(role) {
+      try {
+        await message.member.roles.add(role);
+        console.log('Role Added.')
+        }
+        catch(err) {
+        console.log(err)
+        }
+      }
+    }
   })
 }
 
