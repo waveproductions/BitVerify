4e0a3df5682f86c36739089278099bcafd69db84
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-12 20:26:23 -0400

    Delete Prefix.js

--- models/Prefix.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const PrefixSchema = new mongoose.Schema({
-    GuildPrefix: { type: String, default: 'v!'}
-});
-
-const MessageModel = module.exports = mongoose.model('Prefix', PrefixSchema);
