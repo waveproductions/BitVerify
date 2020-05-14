d21d7529d1b10a1a25dfbc8923ee38ff3bd877f5
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-14 16:23:32 -0400

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -3,13 +3,12 @@ const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
   Stats.findOne({ GuildID: message.guild.id }, async(err, data) => {
-  if(!data) {
-  return message.channel.send('Admins haven\'t set up the verify channel and role yet. Please contact the admins.')
-    }
+  let channel = data.VerifyChannelID || 'Doesn\'t Exist'
+  let role = data.VerifiedRoleID || 'Doesn\'t Exist'
   let embed = new Discord.MessageEmbed()
   .setTitle('Stats')
-  .addField('**Verification Channel**', `<#${data.VerifyChannelID}>`, true)
-  .addField('**Verification Role**', `<#${data.VerifiedRoleID}>`, true)
+  .addField('**Verification Channel**', `<#${channel}>`, true)
+  .addField('**Verification Role**', `<#${role}>`, true)
   .addField('**Server Owner**', `\`\`\`${message.guild.owner.tag}\`\`\``)
   .setThumbnail(message.guild.iconURL())
   .setFooter(`${message.guild.name}'s Stats`)
