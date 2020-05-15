99c0cf66d936ab792330bbdce7ce05fdf49b204d
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-15 11:47:11 -0400

    Update hack.js

--- commands/hack.js
+++ commands/hack.js
@@ -1,6 +1,19 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
+let emails = [
+    "isgay",
+    "thenormie",
+    "veryunepic",
+    "bigsad"
+]
+let email = emails[Math.floor(Math.random()*(emails.length))]
+let domains = [
+    "msn.com",
+    "hotmail.com",
+    "gmail.com"
+]
+let domain = domains[Math.floor(Math.random()*(domains.length))]
 let user = message.mentions.users.first()
 if(!user) {
 return message.channel.send('Who am I going to hack?')
@@ -10,7 +23,10 @@ let msg = message.channel.send('Loading**.**..')
 await msg.edit('Loading.**.**.')
 await msg.edit('Loading..**.**')
 await msg.edit('Getting email...')
-await msg.edit('Email')
+await msg.edit(`Email\: ${user.username}${email}@${domain}`)
+await msg.edit('Stealing cookie pictures...')
+await msg.edit('Injecting virus...')
+await msg.edit('The dangerous hack is complete.')
 }
 
 module.exports.config = {
