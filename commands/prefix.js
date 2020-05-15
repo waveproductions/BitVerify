82adff7ae4bc3a40cfded1af616e288abf89445f
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-15 14:53:06 -0400

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -2,6 +2,11 @@ const Discord = require('discord.js')
 const guildPrefix = require('../models/GuildPrefix')
 
 module.exports.run = async (bot, message, args) => {
+let argsembed = new Discord.MessageEmbed()
+.setTitle('Not Enough Args')
+.setDescription(`❌ Please be more descriptive.
+
+\`v!prefix <new prefix>\``)
 if(!message.member.hasPermission('ADMINISTRATOR')) {
 return message.channel.send('\:x\: You don\'t have permission to use this command.')
 }
