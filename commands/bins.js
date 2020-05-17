c543c9c5ed3093e610f91411308750822cdf6e59
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-17 15:47:14 -0400

    Update bins.js

--- commands/bins.js
+++ commands/bins.js
@@ -5,11 +5,11 @@ module.exports.run = async (bot, message, args) => {
   .setTitle('Bins')
   .setDescription(`**Use these bins instead of pasting code snippets.**
   
-  \`[SourceBin](https://sourceb.in/)\`
-  \`[HasteBin](https://hasteb.in/)\`
-  \`[HateBin](https://hatebin.com/)\`
-  \`[Hastebin](https://hastebin.com/)\`
-  \`[PasteBin](https://pastebin.com/)\``)
+  [SourceBin](https://sourceb.in/)
+  [HasteBin](https://hasteb.in/)
+  [HateBin](https://hatebin.com/)
+  [Hastebin](https://hastebin.com/)
+  [PasteBin](https://pastebin.com/)`)
   .setFooter('I recommend SourceBin.')
   .setColor('GREEN')
   message.channel.send(helpEmbed)
