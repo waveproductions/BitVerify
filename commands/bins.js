c02d4d4284646d5e72a994004ae7763f7e353141
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-17 15:38:48 -0400

    Create bins.js

---
+++ commands/bins.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+  guildPrefix.findOne({ GuildID: message.guild.id }, async(err, data) => {
+  let helpEmbed = new Discord.MessageEmbed()
+  .setTitle('Bins')
+  .setDescription(`**Use these bins instead of pasting code snippets.**
+  
+  \`[SourceBin](https://sourceb.in/)\`
+  \`[HasteBin](https://hasteb.in/)\`
+  \`[HateBin](https://hatebin.com/)\`
+  \`[Hastebin](https://hastebin.com/)\`
+  \`[PasteBin](https://pastebin.com/)\``)
+  .setFooter('I recommend SourceBin.')
+  .setColor('GREEN')
+  message.channel.send(helpEmbed)
+  })
+}
+
+module.exports.config = {
+    name: "bins",
+    description: "A list of bins that you can use instead of pasting code snippets.",
+    usage: "v!bins",
+    accessableby: "All Members",
+    aliases: ['']
+}
