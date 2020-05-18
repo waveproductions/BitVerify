dc475df91e4d353fdce65e86374d1bd4f543da39
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-17 22:29:29 -0400

    Update membercount.js

--- commands/membercount.js
+++ commands/membercount.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   memberCount.findOne({ GuildID: message.guild.id},async(err, data) => {
   let guildcount = bot.guilds.cache.get(message.guild.id)
   if(err) console.log(err)
-  if(!data) {
+  if(!args[0]) {
 let format = new Discord.MessageEmbed()
 .setTitle('Member Count')
 .setDescription(`Please use one of the commands to create a member count channel.
@@ -15,7 +15,11 @@ let format = new Discord.MessageEmbed()
 \`v!membercount bots\` Creates a bot count.
 \`v!membercount human\` Creates a member count of all the real members in your server.`)
 message.channel.send(format)
-  if(args[0] === 'all') {
+  } else 
+if(message.author.bot || message.channel.type === 'dm') {return;}
+        let messageinfocontent = message.content.toLowerCase()
+  switch(args[0]){
+    case 'all':
   let channel = await message.guild.channels.create(`Members\: ${guildcount.memberCount}`, {
   type: 'voice',
   permissionOverwrites: [
@@ -36,34 +40,6 @@ message.channel.send(format)
   .setFooter(`Members\: ${guildcount.memberCount}`)
   .setColor('GREEN')
   }
-    if(args[0] === 'bots') {
-    let channel = await message.guild.channels.create(`Bots\: ${guildcount.members.cache.filter(member => !member.user.bot).size}`, {
-  type: 'voice',
-  permissionOverwrites: [
-     {
-       id: message.guild.id,
-       deny: ['CONNECT'],
-    },
-  ],
-})  
-  let newSettings = new memberCount({
-      CountChannelID: channel.id,
-      GuildID: message.guild.id
-      })
-      newSettings.save()
-  let embed = new Discord.MessageEmbed()
-  .setTitle('Count Created')
-  .setDescription('The bot count channel has been created.')
-  .setFooter(`Bots\: ${guildcount.memberCount}`)
-  .setColor('GREEN')
-    }
-    } else {
-    let existsembed = new Discord.MessageEmbed()
-    .setTitle('Data Already Exists')
-    .setDescription('To add a member count, please use \`v!resetcount\` before using this command.')
-    .setColor('RED')
-    message.channel.send(existsembed)
-    }
   })
 }
 
