03452ba3350dbd7f64b8e4a4aaa4d1d222c56f93
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-17 23:01:04 -0400

    Delete BotCount.js

--- BotCount.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const BotCountSchema = new mongoose.Schema({
-    CountChannelID: String,
-    GuildID: String,
-});
-
-const MessageModel = module.exports = mongoose.model('BotCount', BotCountSchema);
