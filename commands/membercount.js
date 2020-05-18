028b22c296bf4f9861a1b78760e8e577ef3df89c
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-17 22:22:56 -0400

    Update membercount.js

--- commands/membercount.js
+++ commands/membercount.js
@@ -7,7 +7,16 @@ module.exports.run = async (bot, message, args) => {
   let guildcount = bot.guilds.cache.get(message.guild.id)
   if(err) console.log(err)
   if(!data) {
-let channel = await message.guild.channels.create(`Members\: ${guildcount.memberCount}`, {
+let format = new Discord.MessageEmbed()
+.setTitle('Member Count')
+.setDescription(`Please use one of the commands to create a member count channel.
+
+\`v!membercount all\` Creates a member count of all the members in your server.
+\`v!membercount bots\` Creates a bot count.
+\`v!membercount human\` Creates a member count of all the real members in your server.`)
+message.channel.send(format)
+  if(args[0] === 'all') {
+  let channel = await message.guild.channels.create(`Members\: ${guildcount.memberCount}`, {
   type: 'voice',
   permissionOverwrites: [
      {
@@ -26,6 +35,28 @@ let channel = await message.guild.channels.create(`Members\: ${guildcount.member
   .setDescription('The member count channel has been created.')
   .setFooter(`Members\: ${guildcount.memberCount}`)
   .setColor('GREEN')
+  }
+    if(args[0] === 'bots') {
+    let channel = await message.guild.channels.create(`Bots\: ${guildcount.members.cache.filter(member => !member.user.bot).size}`, {
+  type: 'voice',
+  permissionOverwrites: [
+     {
+       id: message.guild.id,
+       deny: ['CONNECT'],
+    },
+  ],
+})  
+  let newSettings = new memberCount({
+      CountChannelID: channel.id,
+      GuildID: message.guild.id
+      })
+      newSettings.save()
+  let embed = new Discord.MessageEmbed()
+  .setTitle('Count Created')
+  .setDescription('The bot count channel has been created.')
+  .setFooter(`Bots\: ${guildcount.memberCount}`)
+  .setColor('GREEN')
+    }
     } else {
     let existsembed = new Discord.MessageEmbed()
     .setTitle('Data Already Exists')
