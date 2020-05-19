5d5027f3c9d1f97e54347db5606ecf28c9868b3e
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-19 17:02:59 -0400

    Create timer.js

---
+++ commands/timer.js
@@ -0,0 +1,59 @@
+const Discord = require('discord.js')
+const ms = require('ms')
+const colors = require('../stuff/colors.json')
+const Timer = require(../models/Timer)
+
+module.exports.run = async(bot, message, args) => {
+Timer.findOne({ MemberID: message.author.id }, async(err, data) => {
+if(!data) {
+let formatembed = new Discord.MessageEmbed()
+.setTitle('Incorrect Format')
+.setDescription(`You did not use a proper format for the time.
+
+\`v!timer <s | m | h | d\``)
+.setColor(colors.green)
+
+    if(!args[0]) {
+    return message.channel.send('You did not specify a time!')
+    }
+    if(!args[0].endsWith("d")){
+        if(!args[0].endsWith("h")){
+            if(!args[0].endsWith("m")){
+                return message.channel.send(`You did not use the proper format for the the time!`)
+            }
+        }
+    }
+    if(isNaN(args[0][0])){
+        return message.channel.send(`That is not a number!`)
+    }
+    let newTimer = new Timer({
+    MemberID: message.author.id,
+    GuildID: message.guild.id,
+    Time: ms(args[0])
+    })
+    newTimer.save()
+    message.channel.send(`${message.author.username}, you have set a timer for ${args[0]}.`)
+    setTimeout(function(){
+    let embed = new Discord.MessageEmbed()
+    .setTitle('Timer Finished')
+    .setDescription(`Your timer for ${args[0]} has finished!`)
+    .setColor(colors.green)
+    message.author.send(embed)
+    Timer.deleteMany
+    }, ms(args[0]))
+    } else {
+    let existsembed = new Discord.MessageEmbed()
+    .setTitle('Timer Already Exists')
+    .setDescription('Please wait until your timer is finished, or use \`v!reset timer\` to reset your timer!')
+    .setColor(colors.red)
+    }
+    })
+}
+
+module.exports.config = {
+    name: "timer",
+    description: "A list of bins that you can use instead of pasting code snippets.",
+    usage: "v!timer <s | m | h | d>",
+    accessableby: "All Members",
+    aliases: ['']
+}
