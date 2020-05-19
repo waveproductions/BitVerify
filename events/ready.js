fcfa951d7d12133bb0480b041a801907c304f3c6
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-18 20:20:01 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -11,7 +11,7 @@
   
   setInterval(function() {
       let status = statuses[Math.floor(Math.random() * statuses.length)];
-      bot.user.setActivity(status, {type: "PLAYING"});
+      bot.user.setPresence({ activity: status, status: dnd });
   }, 2000);
 
 };
