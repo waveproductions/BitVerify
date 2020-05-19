de5fb3783d2fae4186c392641132c76d9c48ca8b
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-19 18:16:40 -0400

    Delete Timer.js

--- models/Timer.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const TimerSchema = new mongoose.Schema({
-    MemberID: String,
-    GuildID: String,
-    Time: String
-});
-
-const MessageModel = module.exports = mongoose.model('Timer', TimerSchema);
