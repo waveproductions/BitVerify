0670a070401f2108ef8d0f76625bebb76a091b6d
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-19 22:28:17 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -17,6 +17,7 @@ module.exports.run = async (bot, message, args) => {
   .addField(`\`${data.prefix}bins\``, 'A list of bins that you can use instead of pasting code snippets.')
   .addField(`\`${data.prefix}hack <mention | id | username>\``, 'A fun command that hacks people.')
   .addField(`\`${data.prefix}say <message>\``, 'Makes the bot say whatever you say.')
+  .addField(`\`${data.prefix}giveaway <s | m | h | d> <channel mention> <prize>\``, 'Starts a giveaway.')
   .setFooter(`Your server's prefix for this bot is ${data.prefix}`)
   .setColor('GREEN')
   message.channel.send(helpEmbed)
