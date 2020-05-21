f71d0ac721722ca2bfd3c7a69ec65d79aa6bc656
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-20 22:57:25 -0400

    Delete DisableXP.js

--- models/DisableXP.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const disableSchema = new mongoose.Schema({
-    GuildID: String,
-});
-
-const MessageModel = module.exports = mongoose.model('DisableXP', disableSchema);
