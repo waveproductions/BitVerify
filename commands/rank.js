9a97c5ad55705b583ec109d1b037777395102686
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-20 21:07:03 -0400

    Create rank.js

---
+++ commands/rank.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const Levels = require('discord-xp')
+
+module.exports.run = async (bot, message, args) => {
+const target = message.mentions.users.first() || message.author;
+
+const user = await Levels.fetch(target.id, message.guild.id);
+
+if (!user) return message.channel.send("This person hasn't gained any XP yet.");
+
+let embed = new Discord.MessageEmbed()
+.setTitle('Rank')
+.setDescription(`<@${user.id}>`)
+.addField('Level', user.level)
+.addField('XP', user.xp)
+message.channel.send(embed);
+}
+
+module.exports.config = {
+    name: "rank",
+    description: "Gets the rank of the person mentioned.",
+    usage: "v!rank <mention>",
+    accessableby: "All Members",
+    aliases: ['']
+}
