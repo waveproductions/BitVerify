1a5d2b04a2af4a1d9fa8e2cbb2aa3e15dabddaf6
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-20 22:42:53 -0400

    Create disablexp.js

---
+++ commands/disablexp.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const mongoose = require('mongoose')
+const disableXP = require(../models/DisableXP)
+
+module.exports.run = async (bot, message, args) => {
+if(!message.member.hasPermission('ADMINISTRATOR')) {
+return message.channel.send('You don\'t have permission to use this command.')
+}
+disableXP.findOne({ GuildID: message.guild.id }, async(err, data) => {
+if(!data) {
+const disable = new disableXP({
+GuildID: message.guild.id
+})
+disable.save()
+    } else {
+    return message.channel.send('Leveling is already disabled!')
+    }
+  })
+}
+
+module.exports.config = {
+    name: "disablexp",
+    description: "Disables leveling.",
+    usage: "v!disablexp",
+    accessableby: "Admins",
+    aliases: ['']
+}
