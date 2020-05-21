c39e7023dcfea2027e9e5e1982166677514d8e38
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-21 10:13:24 -0400

    Create enablexp.js

---
+++ commands/enablexp.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const guildID = require('../models/GuildID')
+
+module.exports.run = async (bot, message, args) => {
+  if(!message.member.hasPermission('ADMINISTRATOR')) {
+  return message.channel.send('You don\'t have permission to use this command.')
+  }
+  
+  guildID.findOne({ GuildID: message.guild.id }, async(err, data) => {
+  if(!data) {
+  return message.channel.send('Leveling is already enabled!')
+  } else {
+  guildID.deleteOne({ GuildID: guild.id }, (err) => console.log(err))
+  message.channel.send('Leveling has been enabled.')
+  }
+  })
+}
+
+module.exports.config = {
+    name: "enablexp",
+    description: "Enables leveling.",
+    usage: "v!enablexp",
+    accessableby: "Admins",
+    aliases: ['']
+}
