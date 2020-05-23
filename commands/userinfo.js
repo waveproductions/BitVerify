1fccc7bbfeb7cde740cc166b4d12938a73465559
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-22 21:24:09 -0400

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const Levels = require('discord-xp')
 
 module.exports.run = async (bot, message, args) => {
-let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
+let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" "))
 let level = await Levels.fetch(member.id, message.guild.id)
 let leveldisplay = ''
 let xpdisplay = ''
