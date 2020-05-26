aee203091714c3c9898245a3a42c5beeb1e0d2ea
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-26 17:46:16 -0400

    Create restart.js

---
+++ commands/restart.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+  if(message.author.id !== '515204641450098704') {
+  return message.channel.send('You are not the owner')
+  } else {
+  let msg = await message.channel.send('Restarting in 10 seconds.')
+  setTimeout(function(){
+          process.exit();
+        }, 10000)
+  }
+}
+
+module.exports.config = {
+    name: "restart",
+    description: "Restarts the bot.",
+    usage: "v!restart",
+    accessableby: "Owner",
+    aliases: ['']
+}
