39f33923e39ba8b82a363f5f610e2be6cb618eb6
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-27 12:29:26 -0400

    Create simprate.js

---
+++ commands/simprate.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+let something = args.slice(0).join(" ")
+let random = Math.round(Math.random() * 100)
+if(!something) {
+something = 'You are'
+}
+if(args.slice(0).join(" ")) {
+something = `${args.slice(0).join(" ")} is`
+}
+let howgayembed = new Discord.MessageEmbed()
+.setTitle('Simp Rate Machine')
+.setDescription(`${something} ${random}% simp.`)
+.setColor('RANDOM')
+message.channel.send(howgayembed)
+}
+
+module.exports.config = {
+    name: "simprate",
+    description: "Calculates your simprate.",
+    usage: "v!simprate <something>",
+    accessableby: "All Members",
+    aliases: ['howsimp', 'simprank']
+}
