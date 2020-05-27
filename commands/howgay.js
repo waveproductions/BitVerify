8e488aa4e834b2c76a14321c743c5235d0642b1b
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-27 12:23:16 -0400

    Create howgay.js

---
+++ commands/howgay.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+let something = args.slice(0).join(" ")
+let random = Math.round(Math.random() * 100)
+if(!something) {
+something = 'You are'
+}
+if(args.slice(0).join(" ")) {
+something = `${args.slice(0).join(" ")} is`
+}
+let howgayembed = new Discord.MessageEmbed()
+.setTitle('Gay Rate Machine')
+.setDescription(`${something} ${random}% gay. \:rainbow_flag\:`)
+.setColor('RANDOM')
+message.channel.send(howgayembed)
+}
+
+module.exports.config = {
+    name: "howgay",
+    description: "Calculates your gayness.",
+    usage: "!howgay <something>",
+    accessableby: "All Members",
+    aliases: ['gayrate']
+}
