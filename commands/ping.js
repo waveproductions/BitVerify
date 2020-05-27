0087894bdff467dfbf1702d1745c31ad67165460
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-27 13:25:28 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
 
 const embed2 = new Discord.MessageEmbed()
-.setTitle('Ping')
-.addField('**API Latency**', `\`${Math.round(bot.ws.ping)}\``)
+.setTitle('🏓 Ping')
+.addField('**API Latency**', `\`${Math.round(bot.ws.ping)} ms\``)
 .setColor('BLUE')
 message.channel.send(embed2)
 }
