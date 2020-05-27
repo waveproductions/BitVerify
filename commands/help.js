61538f5f8b9b9a58ee343bcfd2d31b0463ed7ad8
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-26 21:29:59 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,31 +1,28 @@
 const Discord = require('discord.js')
 const guildPrefix = require('../models/GuildPrefix')
+const fs = require('fs')
 
 module.exports.run = async (bot, message, args) => {
-  guildPrefix.findOne({ GuildID: message.guild.id }, async(err, data) => {
-  let helpEmbed = new Discord.MessageEmbed()
-  .setTitle('BitVerify Command List')
-  .setDescription('\`settings\`, \`verify\`, \`help\`, \`verifyinstructions\`, \`reset\`, \`verifymessage\`, \`membercount\`, \`resetcount\`, \`stats\`, \`bins\`, \`hack\`, \`say\`, \`giveaway\`, \`rank\`, \`leaderboard\`')
-  .addField(`\`${data.prefix}settings <mention channel here> <mention role here>\``, 'Sets the verified role and settings.')
-  .addField(`\`${data.prefix}verify\``, 'Verifies the user.')
-  .addField(`\`${data.prefix}help\``, 'Sends this message!')
-  .addField(`\`${data.prefix}verifyinstructions\``, 'Sends the instructions on how to setup this bot.')
-  .addField(`\`${data.prefix}reset\``, 'Resets your settings just in case you mess up.')
-  .addField(`\`${data.prefix}verifymessage <mention channel here>\``, 'Makes the bot send a message about how to verify so you don\'t have to. Make sure you set the settings before using this command!')
-  .addField(`\`${data.prefix}membercount <channel id>\``, 'Sets the member count.')
-  .addField(`\`${data.prefix}prefix <new prefix>\``, 'Sets a new prefix.')
-  .addField(`\`${data.prefix}bins\``, 'A list of bins that you can use instead of pasting code snippets.')
-  .addField(`\`${data.prefix}hack <mention | id | username>\``, 'A fun command that hacks people.')
-  .addField(`\`${data.prefix}say <message>\``, 'Makes the bot say whatever you say.')
-  .addField(`\`${data.prefix}giveaway <s | m | h | d> <channel mention> <prize>\``, 'Starts a giveaway.')
-  .addField(`\`${data.prefix}leaderboard\``, 'Gets the leaderboard of the server.')
-  .addField(`\`${data.prefix}rank <mention | id>\``, 'Gets the rank of someone.')
-  .addField(`\`${data.prefix}clear <clear amount>\``, 'Clears messages.')
-  .addField(`\`${data.prefix}docs <query>\``, 'Searches the discord.js docs.')
-  .setFooter(`Your server's prefix for this bot is ${data.prefix}`)
-  .setColor('GREEN')
-  message.author.send(helpEmbed)
-  message.channel.send('📩 | A list of commands have been sent to your DMs.')
+  fs.readdir("../commands/", (err, files) => {
+    
+  if(err) console.log(err)
+    
+  let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+    
+  let pull = require(`./commands/${jsfile}`);
+    
+  const commandlist = pull.map(x => `\`${pull.config.name}\``)
+    
+  const mainembed = new Discord.MessageEmbed()
+  .setTitle('BitVerify Commands')
+  .addField('All Commands', `${commandlist.join(", ")}`)
+  .setColor('BLUE')
+  if(!args[0]) {
+  return message.channel.send(mainembed)
+  }
   })
 }
 
