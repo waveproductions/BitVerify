10012e5084eee96aebedb7ee59c7d770fee3e455
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-28 10:38:34 -0400

    Create epicgamerrate.js

---
+++ commands/epicgamerrate.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+let something = args.slice(0).join(" ")
+let random = Math.round(Math.random() * 100)
+if(!something) {
+something = 'You are'
+}
+if(args.slice(0).join(" ")) {
+something = `${args.slice(0).join(" ")} is`
+}
+let howgayembed = new Discord.MessageEmbed()
+.setTitle('Epic Gamer Rate Machine')
+.setDescription(`${something} ${random}% epic gamer.   😎`)
+.setColor('RANDOM')
+message.channel.send(howgayembed)
+}
+
+module.exports.config = {
+    name: "epicgamerrate",
+    description: "See how epic gamer you are.",
+    usage: "v!epicgamerrate <something>",
+    accessableby: "All Members",
+    category: "fun",
+    aliases: ['egrate']
+}
