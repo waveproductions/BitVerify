c6d7259f9f8ebffcc102725be9e26b6e7325bad7
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-28 12:35:00 -0400

    Update pp.js

--- commands/pp.js
+++ commands/pp.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message, args) => {
 
-    const person = message.mentions.users.first() || bot.users.cache.get(args[0]) || message.author
+    const person = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0]) || message.guild.members.cache.get(message.author.id)
     const random = Math.round(Math.random() * 15);
     const ppLevel = "=".repeat(random)
 
     const embed = new Discord.MessageEmbed()
         .setTitle('PP Size Machine')
-        .setDescription(`${person.username}'s Penis
+        .setDescription(`${person.user.username}'s Penis
         8${ppLevel}D`)
         .setColor('RANDOM')
 
