1efb60d50e2e50f2eb12b4e143b3ee9e0ca2896e
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-27 22:25:37 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,17 +9,30 @@ module.exports.run = async (bot, message, args) => {
   if(err) console.log(err)
   
   let cmd = bot.commands.map(c => `\`${c.config.name}\``)
+  let fun = bot.commands.find(x => x.config.category === 'fun')
+  let fun2 = `\`${fun.config.name}\``
   
   const mainembed = new Discord.MessageEmbed()
   .setTitle('BitVerify Commands')
-  .setDescription(`${cmd.join(", ")}`)
+  .addField('🎲 Fun', `\`${data.prefix}help fun\``, true)
+  .addField('🛡️ Moderation', `\`${data.prefix}help moderation\``, true)
+  .addField('🛠️ Utility', `\`${data.prefix}help utility\``, true)
   .setFooter(`Total Commands\: ${bot.commands.size} | Prefix\: ${data.prefix} | Say ${data.prefix}help <command> to get more info about the command.`)
   .setThumbnail(bot.user.displayAvatarURL())
   .setColor('BLUE')
   
   if(!args[0]) {
   return message.channel.send(mainembed)
-  } else {
+  }
+    
+  if(args[0] === 'fun') {
+  let funembed = new Discord.MessageEmbed()
+  .setTitle('🎲 Fun Commands')
+  .setDescription(`${fun2.join(", ")}`)
+  .setFooter(`Total Commands\: ${bot.commands.size} | Prefix\: ${data.prefix} | Say ${data.prefix}help <command> to get more info about the command.`)
+  .setColor('BLUE')
+  return message.channel.send(funembed)
+  }
     
     let command = bot.commands.get(bot.aliases.get(args[0].toLowerCase()) || args[0].toLowerCase())
     if(!command) {
@@ -30,6 +43,7 @@ module.exports.run = async (bot, message, args) => {
   .setTitle(`${command.config.name.slice(0, 1).toUpperCase() + command.config.name.slice(1)} Info`)
   .addField('Name', `${command.config.name.slice(0, 1).toUpperCase() + command.config.name.slice(1)}`)
   .addField('Description', `${command.config.description}`)
+  .addField('Category', `${command.config.category}`)
   .addField('Usage', `\`${command.config.usage}\``)
   .addField('Aliases', `${command.config.aliases.join(", ") ? command.config.aliases.join(", ") : 'None'}`)
   .addField('Accessible By', `${command.config.accessableby}`)
@@ -37,7 +51,6 @@ module.exports.run = async (bot, message, args) => {
   .setThumbnail(bot.user.displayAvatarURL())
   .setColor('BLUE')
   message.channel.send(commandembed)
-  }
   })
     })
 }
