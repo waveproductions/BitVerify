64d8e2da91c51629356ba06b1516040631596b1f
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-29 16:32:44 -0400

    Update contributors.js

--- commands/contributors.js
+++ commands/contributors.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   .setTitle('Bins')
   .setDescription('This is a list of contributors.')
   .addField('Bot Owner', '\`Gavin Vu#5315\` [-](invite.gg/dashcraft) Main Creator of BitVerify')
-  .addField('Others', '\`Poryafm12#6064\` [-](invite.gg/dashcraft) Command Handler\n\`Artist#3818\ [-](invite.gg/dashcraft) Inspiration And Encouragement\n\`Arize#6562\` [-](invite.gg/dashcraft) Commands`')
+  .addField('Others', '\`Poryafm12#6064\` [-](invite.gg/dashcraft) Command Handler\n\`Artist#3818\` [-](invite.gg/dashcraft) Inspiration And Encouragement\n\`Arize#6562\` [-](invite.gg/dashcraft) Commands`')
   .setColor('BLUE')
   .setFooter('The best people I have met.')
   message.channel.send(helpEmbed)
