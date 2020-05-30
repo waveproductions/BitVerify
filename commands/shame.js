81aabb40aaea0f33a4b563be1bbb6c3d90138124
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-30 17:48:53 -0400

    Update shame.js

--- commands/shame.js
+++ commands/shame.js
@@ -2,6 +2,9 @@ const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
+    if(!member) {
+    return message.channel.send('How am I going to shame nobody?')
+    }
     message.channel.send(`🔔 SHAME 🔔 ${member} 🔔 SHAME 🔔`)
 }
 
