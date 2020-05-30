6cd5363f91505d0662142375cb7df46fa8916473
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-29 22:09:03 -0400

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     const snipes = bot.snipes.get(message.channel.id) || []
     const msg = snipes[args[0] - 1 || 0];
     if (!msg) return message.channel.send(`That snipe doesn't exist.`);
-    const Embed = new MessageEmbed()
+    const Embed = new Discord.MessageEmbed()
       .setAuthor(
         msg.author.tag,
         msg.author.displayAvatarURL({ dynamic: true, size: 256 })
