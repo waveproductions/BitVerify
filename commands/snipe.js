2c14c148ac3e5adc3346d1a06d6d833cdfb31237
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-29 22:11:43 -0400

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -2,15 +2,15 @@ const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
     const snipes = bot.snipes.get(message.channel.id) || []
-    const msg = snipes[args[0] - 1 || 0];
-    if (!msg) return message.channel.send(`That snipe doesn't exist.`);
+    const msg = snipes[0];
+    if (!msg) return message.channel.send(`No messages have been deleted yet.`);
     const Embed = new Discord.MessageEmbed()
       .setAuthor(
         msg.author.tag,
         msg.author.displayAvatarURL({ dynamic: true, size: 256 })
       )
       .setDescription(msg.content)
-      .setFooter(`Date: ${msg.date} | ${args[0] || 1}/${snipes.length}`);
+      .setFooter(`${msg.date}`);
     if (msg.attachment) Embed.setImage(msg.attachment);
     message.channel.send(Embed);
 }
