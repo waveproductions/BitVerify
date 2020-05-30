dbc0b18ef86fc6d0c026c7296b36218734f3d47a
Gavin Vu <62964708+gavserve@users.noreply.github.com>
2020-05-30 17:46:41 -0400

    Create shame.js

---
+++ commands/shame.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
+    message.channel.send(`🔔 SHAME 🔔 ${member} 🔔 SHAME 🔔`)
+}
+
+module.exports.config = {
+    name: "shame",
+    description: "Shame.",
+    usage: "v!shame <member>",
+    accessableby: "All Members",
+    category: "fun",
+    cooldown: 2,
+    aliases: []
+}
