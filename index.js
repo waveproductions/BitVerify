63e98a8d8beb1867fa8fdb13026e07df03853d79
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-02 19:22:17 -0400

    guildDelete

--- index.js
+++ index.js
@@ -289,6 +289,7 @@ bot.on('guildDelete', guild => {
     humanCount.deleteOne({ GuildID: guild.id }, (err) => console.log(err))
     placeholder.deleteMany({ GuildID: guild.id }, (err) => console.log(err))
     cookies.deleteMany({ GuildID: guild.id }, (err) => console.log(err))
-    })
+    logChannel.deleteOne({ GuildID: guild.id }, (err) => console.log(err))
+  });
 
 bot.login(process.env.token)
