d53467b93463b48c6ca42308cc47501b05362a6e
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-01 21:00:16 -0400

    New Kill Message

--- commands/kill.js
+++ commands/kill.js
@@ -2,15 +2,15 @@ const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
-    
+
     if(!member) {
     return message.channel.send('That person doesn\'t exist.')
     }
-    
+
     if(!args[0]) {
     return message.channel.send('How can I kill nobody?')
     }
-    
+
     let replies = [
     `${member.user.username} dies in the hands of ${message.author.username}.`,
     `${member.user.username} dies to an angry mob of feminists.`,
@@ -19,9 +19,10 @@ module.exports.run = async (bot, message, args) => {
     `${member.user.username} died because they drank too much water.`,
     `${member.user.username} dies because they suffered from depression since they were 4.`,
     `${member.user.username} lives somehow.`,
-    `${member.user.username} stays up to late and their parents stick a chopstick up their a**.`
+    `${member.user.username} stays up too late and their parents stick a chopstick up their a**.`,
+    `${member.user.username} read the discord.js docs too much.`
     ];
-    
+
     if(message.author.id === member.id) {
     return message.channel.send(`${message.author.username} killed themself.`)
     }
