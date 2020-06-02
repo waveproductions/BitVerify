908cc10894dc7c00e1732f62133d133ae687feb7
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-02 19:04:19 -0400

    Forgot Comma

--- util/eventHandler.js
+++ util/eventHandler.js
@@ -2,5 +2,5 @@ const reqEvent = (event) => require(`../events/${event}`)
 
 module.exports = bot => {
     bot.on("ready", function() {reqEvent("ready") (bot) });
-    bot.on("messageDelete" function() {reqEvent("messageDelete") (message) });
+    bot.on("messageDelete", function() {reqEvent("messageDelete") (message) });
  }
