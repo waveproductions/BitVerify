16e453982fe7d8bff98aa766851c52bbf1e759b6
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-01 21:08:43 -0400

    Updated contributors.js

--- commands/contributors.js
+++ commands/contributors.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   .setTitle('Contributors')
   .setDescription('This is a list of contributors.')
   .addField('Bot Owner', '\`Gavin Vu#5315\` [-](https://github.com/gavserve/BitVerify/edit/master/commands/contributors.js) Main Creator of BitVerify')
-  .addField('Others', '\`Poryafm12#6064\` [-](https://github.com/gavserve/BitVerify/edit/master/commands/contributors.js) Command Handler and his big brain\n\`Artist#3818\` [-](https://github.com/gavserve/BitVerify/edit/master/commands/contributors.js) Inspiration And Encouragement\n\`Arize#6562\` [-](https://github.com/gavserve/BitVerify/edit/master/commands/contributors.js) Commands')
+  .addField('Others', '\`Poryafm12#6064\` [-](https://github.com/gavserve/BitVerify/edit/master/commands/contributors.js) Command Handler and his Big Brain\n\`Artist#3818\` [-](https://github.com/gavserve/BitVerify/edit/master/commands/contributors.js) Inspiration And Encouragement\n\`Arize#6562\` [-](https://github.com/gavserve/BitVerify/edit/master/commands/contributors.js) Commands\n\`Garden#5665\` [-](https://github.com/gavserve/BitVerify/edit/master/commands/contributors.js) Really Big Brain')
   .setColor('BLUE')
   .setFooter('The best people I have met.')
   message.channel.send(helpEmbed)
