545d92fa67a922534aaf0c49ef222735beb13268
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-03 20:38:50 -0400

    Canvas

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js')
 const Levels = require('discord-xp')
 const cookies = require('../models/Cookies')
+const Canvas = require('canvas')
 
 module.exports.run = async (bot, message, args) => {
-message.channel.send('WORK IN PROGRESS')
+  const canvas = Canvas.createCanvas(350, 500);
+  const ctx = canvas.getContext('2d');
+  const background = await Canvas.loadImage('../image/gray2.PNG');
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'profile.png');
+message.channel.send('WORK IN PROGRESS', attachment)
 }
 
 module.exports.config = {
