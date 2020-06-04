4735637bfab32cdcaf1655b7cd971f19d25c6a34
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-04 11:03:34 -0400

    Text 11

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -32,9 +32,9 @@ module.exports.run = async (bot, message, args) => {
   ctx.fillStyle = "white"
   ctx.fillRect(0, 0, 1000, 380)
 
-  ctx.font = applyText(canvas, member.user.sername)
+  ctx.font = applyText(canvas, member.user.tag);
   ctx.fillStyle = "white";
-  ctx.fillText(member.user.tag, canvas.width / 3.45, 565)
+  ctx.fillText(member.user.tag, canvas.width / 3.45, 565);
 
   ctx.beginPath();
   ctx.arc(500, 380, 120, 0, Math.PI * 2, true);
