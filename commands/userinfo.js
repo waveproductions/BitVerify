d09c3075445b141d68ec729e561d4f2cb1c39a2a
gavserve <62964708+gavserve@users.noreply.github.com>
2020-06-07 11:39:06 -0400

    New Font 2

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -3,7 +3,7 @@ const Levels = require('discord-xp')
 const cookies = require('../models/Cookies')
 const Canvas = require('canvas')
 
-Canvas.registerFont('././otherfonts/Minecraftia-Regular.tff', { family: 'Minecraft' })
+Canvas.registerFont('../otherfonts/Minecraftia-Regular.tff', { family: 'Minecraft' })
 
 const applyText = (canvas, text) => {
 	const ctx = canvas.getContext('2d');
